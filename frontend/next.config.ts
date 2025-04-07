cff477bc756002caea955f3185a92fd3acc68dde
CinnamonfirJ <cinnamonfir@gmail.com>
2025-04-07 09:29:15 +0100

    Fixed next.js framework 404 error from vercel options

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,15 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  // async rewrites() {
-  //   return [
-  //     {
-  //       source: "/api/:path*", // Intercept all /api requests
-  //       // destination: "http://localhost:10000/api/:path*", // Redirect to backend
-  //       destination: "https://kwikly.onrender.com/api/:path*", // Redirect to backend
-  //     },
-  //   ];
-  // },
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*", // Intercept all /api requests
+        // destination: "http://localhost:10000/api/:path*", // Redirect to backend
+        destination: "https://kwikly.onrender.com/api/:path*", // Redirect to backend
+      },
+    ];
+  },
   images: {
     remotePatterns: [
       {
